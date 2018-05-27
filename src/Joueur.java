d6c9efc83de6753cb657c130336da0a2006711f2
Gak47bosniak <gaelchereau@gmail.com>
2018-05-27 19:08:23 +0200

    ere

--- src/Joueur.java
+++ src/Joueur.java
@@ -19,25 +19,6 @@ public class Joueur {
 			this.Mission = ListeMissions.get(a);
 		}
 	
-	//Attribution des territoires à chaque joueur
-	public void AttribuerTerritoires(ArrayList<Joueur> ListeJoueur,ArrayList<Territoire> ListeTerritoires) {
-			int nbJoueurs = ListeJoueur.size();
-			int nbTerritoireMax = ListeTerritoires.size()/nbJoueurs;//On calcule le nombre de territoires à attribuer à chaque joueur (division euclidienne sans prendre en compte le reste)
-			ArrayList<Territoire> ListeTerritoiresRestants = new ArrayList<Territoire>();//On créé une liste avec tous les territoires pour pouvoir la modifier en gardant ListeTerritoires intacte
-			ListeTerritoiresRestants.addAll(ListeTerritoires);
-			
-			for(int i=0; i<nbTerritoireMax; i++) {//On créé une boucle qui se repetera le nombre de foix qu'il y aura de territoires à attribuer aux joueurs
-				
-				int y= GenererNbAleatoire(0, ListeTerritoiresRestants.size()-1);//On prend un nombre aléatoire dans les indexes de la liste des territoires à attribuer
-				TerritoiresJoueur.add(ListeTerritoiresRestants.get(y));//On l'ajoute à la liste des territoires du joueur en question
-				ListeTerritoiresRestants.remove(ListeTerritoiresRestants.get(y));//Et on la supprime de la liste des territoires à attribuer car il vient d'etre attribué
-				//Test qui affiche le nom du joueur à qui on attribue le territoire et le territoire que l'on lui attribu
-				System.out.println(this.acronyme);
-				System.out.println(ListeTerritoires.get(y).toString());
-			}
-			
-		}
-	
 	//Fonction generer nb aléat
 	public static int GenererNbAleatoire(int a, int b) {
 		int nombreAleatoire = a + (int)(Math.random() * ((b - a) + 1));
